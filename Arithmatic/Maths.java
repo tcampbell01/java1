59e003808ce700a23910005863f7fbf778a8b3e8
Teresa Campbell <teresacampbell@Teresas-MacBook-Air.local>
2022-08-02 10:50:39 -0400

    Maths works

---
+++ Arithmatic/Maths.java
@@ -0,0 +1,23 @@
+package Arithmatic;
+
+public class Maths {
+    
+    public static void main(String args[])
+
+{
+
+    int num1 = 5, num2 =7, Result1, Result2, Result4;
+
+    Result1 = num1 + num2;
+    Result2 = num1 * num2;
+    float Result3 = num2 % num1;
+    Result4 = num2 - num1;
+
+    System.out.println("Addition of Result is " + Result1);
+    System.out.println("Multiplication of Result is " + Result2);
+    System.out.println("Division of Result is " + Result3);
+    System.out.println("Subtraction of Result is " + Result4);
+
+}
+
+}
