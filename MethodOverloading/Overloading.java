2a6bc1ee8733c126a1456eb9d4a45bf06cca0c3b
Teresa Campbell <teresacampbell@Teresas-MacBook-Air.local>
2022-08-02 15:35:27 -0400

    added comments

--- MethodOverloading/Overloading.java
+++ MethodOverloading/Overloading.java
@@ -1,26 +1,27 @@
+//in Java, two or more methods may have the same name if they differ in parameters (different number of parameters, different types of parameters, or both). These methods are called overloaded methods and this feature is called method overloading.
+
 package MethodOverloading;
+
+//The Java I/O package, a.k.a. java.io, provides a set of input streams and a set of output streams used to read and write data to files or other input and output sources.
+
 import java.io.*;
 
-
 public class Overloading {
-    int sum(int num1, int num2)
-    {
-        return(num1*num2);
+    int sum(int num1, int num2) {
+        return (num1 * num2);
     }
 }
 
-    class MethodCall
-    {
-        public static void main(String args[]) throws NumberFormatException, IOException
-        {
-            int x,y;
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+class MethodCall {
+    public static void main(String args[]) throws NumberFormatException, IOException {
+        int x, y;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-            System.out.println("Please, enter two Integers: ");
-            x = Integer.parseInt(br.readLine());
-            y = Integer.parseInt(br.readLine());
+        System.out.println("Please, enter two Integers: ");
+        x = Integer.parseInt(br.readLine());
+        y = Integer.parseInt(br.readLine());
 
-            Overloading o1 = new Overloading();
-            System.out.println("Result of your numbers are : " + o1.sum(x,y));
-        }
+        Overloading o1 = new Overloading();
+        System.out.println("Result of your numbers are : " + o1.sum(x, y));
     }
+}
